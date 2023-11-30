5f26f70e20fccb33bf7f7e41b9e2b5ec88ff341e
henrikmdev <henrik@henrikmdev.com>
2023-11-29 21:16:19 -0500

    Make result variable random

--- magic8ball.c
+++ magic8ball.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
     int result = 1;
     printf("Welcome to the Random Dice Roll Simulator!\n");
+
+    // Seed the random number generator
+    srand(time(0));
+
+    // Roll a six-sided die
+    result = rand() % 6 + 1;
 
     printf("You rolled a: %d\n", result);
 
