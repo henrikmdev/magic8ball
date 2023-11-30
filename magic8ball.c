939e1e588779d70948c0321e2c380c1dc1dd02fc
henrikmdev <henrik@henrikmdev.com>
2023-11-29 21:09:48 -0500

    Add initial source code

---
+++ magic8ball.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int result = 1;
+    printf("Welcome to the Random Dice Roll Simulator!\n");
+
+    printf("You rolled a: %d\n", result);
+
+    return 0;
+}
